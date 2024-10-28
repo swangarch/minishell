5f9419319ee8f876c963f559e0854e1a33e0c0d9
Shuai Wang <shuwang@paul-f4Ar7s7.clusters.42paris.fr>
2024-10-28 18:53:30 +0100

    Add ignore file

--- src_sw/main/testcase.sh
+++
@@ -1,7 +0,0 @@
-./minishell
-
-ls -a |cat -e|echo "Hel'lo"world"test" >> check|grep $A >> "'cbsd  .txt'  "Hello""
-
-ls >> test >> test2 -a -l| grep -oE << infile < hello test|echo "Hello world">>test>check
-
-ls >> test >> test2 -a -l| grep -oE << infile < hello test|echo "Hello world">>test>check | ls -a |cat -e|echo "Hel'lo"world"test" >> check|grep $A >> "'cbsd  .txt'  "Hello""
