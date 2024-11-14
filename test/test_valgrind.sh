dc348034fb8f75dfa10a48b37604e54370001cc2
Shuai Wang <shuwang@paul-f4Ar7s8.clusters.42paris.fr>
2024-11-14 14:15:32 +0100

    Final version

--- test/test_valgrind.sh
+++
@@ -1,4 +0,0 @@
-cd ../minishell
-make
-
-valgrind --leak-check=full --suppressions=../test/readline.supp --show-leak-kinds=all --track-fds=yes --trace-children=yes ./minishell
--- test/test_valgrind_sw.sh
+++
@@ -1,4 +0,0 @@
-cd ../minishell_sw
-make
-
-valgrind --leak-check=full --suppressions=../test/readline.supp --show-leak-kinds=all --track-fds=yes --trace-children=yes ./minishell
